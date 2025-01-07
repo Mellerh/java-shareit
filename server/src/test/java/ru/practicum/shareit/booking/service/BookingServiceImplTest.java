8866dcde2425bbcdc4ac9b7ee7d0896185963477
Mellerh <103604450+Mellerh@users.noreply.github.com>
2025-01-07 20:01:42 +0300

    07.01 дорабатываю тесты

---
+++ server/src/test/java/ru/practicum/shareit/booking/service/BookingServiceImplTest.java
@@ -0,0 +1,26 @@
+package ru.practicum.shareit.booking.service;
+
+import org.junit.jupiter.api.Test;
+
+public class BookingServiceImplTest {
+
+    @Test
+    void getBookingById() {
+    }
+
+    @Test
+    void getAllUserBookings() {
+    }
+
+    @Test
+    void getAllUserBookingsItems() {
+    }
+
+    @Test
+    void addNewBooking() {
+    }
+
+    @Test
+    void approveBooking() {
+    }
+}
