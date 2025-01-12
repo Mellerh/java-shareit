f2b95560284d38ce88904dbc989e643a32a2f72d
Mellerh <103604450+Mellerh@users.noreply.github.com>
2025-01-12 14:18:12 +0300

    12.01 исправляю ошибки pr

--- gateway/src/main/java/ru/practicum/shareit/item/client/ItemClient.java
+++ gateway/src/main/java/ru/practicum/shareit/item/client/ItemClient.java
@@ -18,7 +18,6 @@ public class ItemClient extends BaseClient {
 
     private static final String API_PREFIX = "/items";
 
-    
     @Autowired
     public ItemClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
         super(
